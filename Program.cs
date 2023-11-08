9b2d598f93a38fe213e51d1905f4d7328ec86319
rot <appl.rot13@gmail.com>
2023-11-08 11:26:34 +0900

    Create new

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ListHashesPerformanceTest>();
+
+[MemoryDiagnoser]
+public class ListHashesPerformanceTest
+{
+    private static readonly string TargetDirPath = AppContext.BaseDirectory;
+    private static readonly HashAlgorithm HashAlgorithm = SHA1.Create();
+
+    [Benchmark]
+    public List<(string, string)> WithLocksTest()
+    {
+        return Test(ComputeHashWithLocks);
+    }
+
+    [Benchmark]
+    public List<(string, string)> WithoutLocksTest()
+    {
+        return Test(ComputeHashWithoutLocks);
+    }
+
+    private static List<(string, string)> Test(Func<string, byte[]> computeHash)
+    {
+        return Directory.EnumerateFiles(TargetDirPath, "*", SearchOption.AllDirectories).AsParallel()
+            .Select(filePath => (filePath, ToHexString(computeHash(filePath)))).OrderBy(t => t.filePath).ToList();
+    }
+
+    private static byte[] ComputeHashWithLocks(string filePath)
+    {
+        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        {
+            lock (HashAlgorithm)
+            {
+                return HashAlgorithm.ComputeHash(stream);
+            }
+        }
+    }
+
+    private static byte[] ComputeHashWithoutLocks(string filePath)
+    {
+        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        using (var hashAlgorithm = SHA1.Create())
+        {
+            return hashAlgorithm.ComputeHash(stream);
+        }
+    }
+
+    private static string ToHexString(byte[] bytes)
+    {
+        //return BitConverter.ToString(bytes).Replace("-", string.Empty).ToLower();
+        return Convert.ToHexString(bytes).ToLower();
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+List Hashes Performance Test
+============================
+
+Result
+------
+
+```
+BenchmarkDotNet v0.13.10, Windows 10 (10.0.19045.3636/22H2/2022Update)
+Intel Core i7-10700 CPU 2.90GHz, 1 CPU, 16 logical and 8 physical cores
+.NET SDK 8.0.100-rc.2.23502.2
+  [Host]     : .NET 7.0.13 (7.0.1323.51816), X64 RyuJIT AVX2
+  DefaultJob : .NET 7.0.13 (7.0.1323.51816), X64 RyuJIT AVX2
+```
+
+| Method           | Mean      | Error    | StdDev   | Allocated |
+|----------------- |----------:|---------:|---------:|----------:|
+| WithLocksTest    | 159.02 ms | 0.581 ms | 0.515 ms | 264.01 KB |
+| WithoutLocksTest |  33.92 ms | 0.674 ms | 0.988 ms | 274.52 KB |
